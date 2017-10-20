492ff346eb0d5b3634ed43a7f5efafef94b514ae
minnowaka <kshsk11@naver.com>
2017-10-20 11:24:40 +0900

    first commit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("모");
+		System.out.println("르");
+		System.out.println("겟");
+		System.out.println("다");
+
+	}
+
+}
